2090b12956112d1ad00e3c7ebcc4ca96696a496c
CihanDikkaya <cihandikkaya09@hotmail.com>
2023-09-23 18:10:40 +0300

    DTO devam

---
+++ EasyCash.DTOLayer/DTOS/AppUserDTO/AppUserRegisterDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCash.DTOLayer.DTOS.AppUserDTO
+{
+    public class AppUserRegisterDTO
+    {
+        //[Required(ErrorMessage = "Ad alanını boş geçemezsiniz!")]
+        //[Display(Name = "İsim:")]
+        //[MaxLength(30, ErrorMessage = "En fazla 30 karakter girilmelidir.")]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+    }
+}
