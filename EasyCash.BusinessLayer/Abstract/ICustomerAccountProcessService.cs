c174304004e7a31c21f9e5f1005a30445a2aa65e
CihanDikkaya <cihandikkaya09@hotmail.com>
2023-09-24 00:44:53 +0300

    Tema dosyaları ve manager yazımı

---
+++ EasyCash.BusinessLayer/Abstract/ICustomerAccountProcessService.cs
@@ -0,0 +1,15 @@
+﻿using EasyCash.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCash.BusinessLayer.Abstract
+{
+    public interface ICustomerAccountProcessService:IGenericService<CustomerAccountProcess>
+    {
+
+    }
+}
