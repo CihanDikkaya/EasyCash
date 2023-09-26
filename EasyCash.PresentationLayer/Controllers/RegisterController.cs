7ec5aa926e77f239863496170f9e02b92c04dd10
CihanDikkaya <cihandikkaya09@hotmail.com>
2023-09-26 14:29:32 +0300

    Kayıt olma sayfası

--- EasyCash.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCash.PresentationLayer/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
-﻿using EasyCash.EntityLayer.Concrete;
+﻿using EasyCash.DTOLayer.DTOS.AppUserDTO;
+using EasyCash.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,16 +7,40 @@ namespace EasyCash.PresentationLayer.Controllers
 {
     public class RegisterController : Controller
     {
-        private readonly UserManager<AppUser> _usermanager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public RegisterController(UserManager<AppUser> usermanager)
+        public RegisterController(UserManager<AppUser> userManager)
         {
-            _usermanager = usermanager;
+            _userManager = userManager;
         }
 
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDTO appUserRegisterDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    UserName = appUserRegisterDTO.Username,
+                    Name = appUserRegisterDTO.Name,
+                    Surname = appUserRegisterDTO.Surname,
+                    Email = appUserRegisterDTO.Email
+                };
+                var result = await _userManager.CreateAsync(appUser, appUserRegisterDTO.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "ConfirmMail");
+                }
+           
+            }
+            return View();
+        }
+
     }
 }
