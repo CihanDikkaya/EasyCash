32193e207e3d67093cbd11a88480e5c92648058a
CihanDikkaya <cihandikkaya09@hotmail.com>
2023-10-02 17:01:00 +0300

    Kayıt formu sayfasında şifreler için uyarı mesajı çıkarttım.

--- EasyCash.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCash.PresentationLayer/Controllers/RegisterController.cs
@@ -5,42 +5,52 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCash.PresentationLayer.Controllers
 {
-    public class RegisterController : Controller
-    {
-        private readonly UserManager<AppUser> _userManager;
+	public class RegisterController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
 
-        public RegisterController(UserManager<AppUser> userManager)
-        {
-            _userManager = userManager;
-        }
+		public RegisterController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
 
 
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> Index(AppUserRegisterDTO appUserRegisterDTO)
-        {
-            if (ModelState.IsValid)
-            {
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Index(AppUserRegisterDTO appUserRegisterDTO)
+		{
+			if (ModelState.IsValid)
+			{
 				AppUser appUser = new AppUser()
-                {
-                    UserName = appUserRegisterDTO.Username,
-                    Name = appUserRegisterDTO.Name,
-                    Surname = appUserRegisterDTO.Surname,
-                    Email = appUserRegisterDTO.Email
-                };
-                var result = await _userManager.CreateAsync(appUser, appUserRegisterDTO.Password);
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index", "ConfirmMail");
-                }
-           
-            }
-            return View();
-        }
+				{
+					UserName = appUserRegisterDTO.Username,
+					Name = appUserRegisterDTO.Name,
+					Surname = appUserRegisterDTO.Surname,
+					Email = appUserRegisterDTO.Email,
+					City = "aaaaaaa",
+					District = "bbbbbb",
+					ImageURL = "cccccccc"
+				};
+				var result = await _userManager.CreateAsync(appUser, appUserRegisterDTO.Password);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index", "ConfirmMail");
+				}
+				else
+				{
+					foreach (var item in result.Errors)
+					{
+						ModelState.AddModelError("", item.Description);
+					}
+				}
 
-    }
+			}
+			return View();
+		}
+
+	}
 }
