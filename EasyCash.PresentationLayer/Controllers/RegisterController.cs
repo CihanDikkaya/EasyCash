3994ef5a764d756983fe9a4a0bb24804d58aa17c
CihanDikkaya <cihandikkaya09@hotmail.com>
2023-10-05 16:15:55 +0300

    Onay Postası ayarlama

--- EasyCash.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCash.PresentationLayer/Controllers/RegisterController.cs
@@ -1,7 +1,9 @@
 ﻿using EasyCash.DTOLayer.DTOS.AppUserDTO;
 using EasyCash.EntityLayer.Concrete;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 
 namespace EasyCash.PresentationLayer.Controllers
 {
@@ -25,6 +27,8 @@ namespace EasyCash.PresentationLayer.Controllers
 		{
 			if (ModelState.IsValid)
 			{
+				Random random = new Random();
+				int code = random.Next(100000, 1000000);
 				AppUser appUser = new AppUser()
 				{
 					UserName = appUserRegisterDTO.Username,
@@ -33,11 +37,35 @@ namespace EasyCash.PresentationLayer.Controllers
 					Email = appUserRegisterDTO.Email,
 					City = "aaaaaaa",
 					District = "bbbbbb",
-					ImageURL = "cccccccc"
+					ImageURL = "cccccccc",
+					Confirmcode = code
 				};
 				var result = await _userManager.CreateAsync(appUser, appUserRegisterDTO.Password);
 				if (result.Succeeded)
 				{
+					MimeMessage mimeMessage = new MimeMessage();
+					MailboxAddress mailboxAddressFrom = new MailboxAddress("Easy Cash Admin", "cihandikkaya09@gmail.com");
+					MailboxAddress mailboxAddressTo = new MailboxAddress("User", appUser.Email);
+
+					mimeMessage.From.Add(mailboxAddressFrom);
+					mimeMessage.To.Add(mailboxAddressTo);
+					var bodyBuilder = new BodyBuilder();
+					bodyBuilder.TextBody = "Kayıt işlemini gerçekleştirmek için Onay Kodunuz:" + code;
+					mimeMessage.Body = bodyBuilder.ToMessageBody();
+					mimeMessage.Subject = "Easy Cash Onay Kodu";
+					SmtpClient client= new SmtpClient();
+					client.Connect("smtp.gmail.com", 587, false);
+					client.Authenticate("cihandikkaya09@gmail.com", "ewxc rrdg vfpj rskj");
+					client.Send(mimeMessage);
+					client.Disconnect(true);
+
+
+
+
+
+
+
+
 					return RedirectToAction("Index", "ConfirmMail");
 				}
 				else
