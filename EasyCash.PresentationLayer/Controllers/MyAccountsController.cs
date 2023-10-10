d15d4274e57fd62b23f883ff7bab319a7e8d8329
CihanDikkaya <cihandikkaya09@hotmail.com>
2023-10-11 00:52:58 +0300

    Kullanıcı bilgileri getirmeye devam.

--- EasyCash.PresentationLayer/Controllers/MyAccountsController.cs
+++ EasyCash.PresentationLayer/Controllers/MyAccountsController.cs
@@ -1,4 +1,5 @@
-﻿using EasyCash.EntityLayer.Concrete;
+﻿using EasyCash.DTOLayer.DTOS.AppUserDTO;
+using EasyCash.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -18,7 +19,16 @@ namespace EasyCash.PresentationLayer.Controllers
         public async Task<IActionResult> Index()
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
-            return View();
+            AppUserEditDTO appUserEditDTO = new AppUserEditDTO();
+            appUserEditDTO.Name = values.Name;
+            appUserEditDTO.Surname = values.Surname;
+            appUserEditDTO.PhoneNumber = values.PhoneNumber;
+            appUserEditDTO.ImageURL = values.ImageURL;
+            appUserEditDTO.Email = values.Email;
+            appUserEditDTO.City = values.City;
+            appUserEditDTO.District = values.District;
+
+            return View(appUserEditDTO);
         }
     }
 }
