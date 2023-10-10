ffd3d835f212b501b0c6889e500d617220494dac
CihanDikkaya <cihandikkaya09@hotmail.com>
2023-10-10 11:42:31 +0300

    UI Düzenlemesine devam.

---
+++ EasyCash.PresentationLayer/Controllers/MyAccountsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCash.PresentationLayer.Controllers
+{
+    public class MyAccountsController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
