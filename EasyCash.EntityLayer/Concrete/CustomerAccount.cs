6a09ef05b9a719075f2864b76c39b106d6f33c45
CihanDikkaya <cihandikkaya09@hotmail.com>
2023-10-11 14:05:05 +0300

    Müşteri Tablosu yeniden değerlendirildi.

--- EasyCash.EntityLayer/Concrete/CustomerAccount.cs
+++ EasyCash.EntityLayer/Concrete/CustomerAccount.cs
@@ -19,6 +19,9 @@ namespace EasyCash.EntityLayer.Concrete
 
         public int AppUserID { get; set; }
         public AppUser AppUser { get; set; }
+        public List<CustomerAccountProcess> CustomerSender { get; set; }
+        public List<CustomerAccountProcess> CustomerReceiver { get; set; }
+
 
 
 
